5df4db7c56f06588a2627dde9259ab606b4c55ef
Julia Brouard <julia.brouard1@gmail.com>
2019-02-14 17:08:58 +0100

    cards created

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,17 +5,8 @@ class DosesController < ApplicationController
   end
 
   def create
-    # Fetch the cocktail from DB
-    cocktail = Cocktail.find(params[:cocktail_id])
-    # Fetch the ingredient from DB
-    #ingredient = Ingredient.find(params[:dose][:ingredient_id])
-    # Create a new dose from params
     @dose = Dose.new(dose_params)
-    # Link the cocktail and the dose
-    @dose.cocktail = cocktail
-    # Link the ingredient and the dose
-    #@dose.ingredient = ingredient
-    # Save the dose
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
